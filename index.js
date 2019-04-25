d8c582ad6a139f912660c9ee0a9a926c45cb15ac
msjawad <solaimanjawad@gmail.com>
2019-04-25 00:24:47 -0400

    pushing the url socket to post msg

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const fs = require('fs');
-const torrent = fs.readFileSync('puppy.torrent');
+const bencode = require('bencode');
 
-console.log(torrent.toString('utf8'));
+const dgram = require('dgram');
+const urlParse = require ('url').parse;
+const buffer = require('buffer').Buffer;
+
+
+// decode the bencode serializationed string into someting legitble, get the
+// trackers
+const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
+
+const url = urlParse(torrent.announce.toString('utf8'));
+
+const socket = dgram.createSocket('udp4');
+
+const newMsg = buffer.from('hello', 'utf8');
+
+socket.send(newMsg, 0, newMsg.length, url.port, url.host, () => {});
+
+socket.on('message', msg => {
+    console.log('my message is', msg);
+});
+
 
 
